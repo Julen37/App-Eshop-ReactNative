fa2279038ab508dc887e279f972746518f595a3d
Julen37 <julen.gouchault@gmail.com>
2025-11-12 15:24:44 +0100

    interface product - type.ts

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import HomeHeader from '@/components/HomeHeader';
+import { useState } from 'react';
 import { 
   StyleSheet, Text, 
   View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
+  const [featuredProducts, setFeaturedProducts] = useState([]);
   return (
       <View>
         <HomeHeader/>
